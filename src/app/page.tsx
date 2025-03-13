8f3372b4ba2f0e5229b684ce45c2ce4a99ba189b
Kully <deeptech@it.kmitl.ac.th>
2025-03-13 20:28:46 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import NotificationButton from "@/components/NotificationButton";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>FCM Notification</h1>
+      <NotificationButton />
+    </div>
+  );
+}
